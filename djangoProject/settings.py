820a025f35c118b6272b4d0ff5ef4d953cee2992
Vadim Perfilev <ifff1lya@gmail.com>
2021-05-16 00:49:27 +0300

    connect db

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'project',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'DESKTOP-QNVS6M3',
+        'PORT': '',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+            'host_is_server': True
+        }
     }
 }
 
